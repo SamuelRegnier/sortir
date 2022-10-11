b57144c56262e8c0b892983acdf74ad475382fd5
tmasse2022 <tony.masse2022@campus-eni.fr>
2022-10-11 14:45:03 +0200

    Page d'accueil en cours

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Sortie;
+use App\Repository\SortieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,10 +12,19 @@ use Symfony\Component\Routing\Annotation\Route;
 class AccueilController extends AbstractController
 {
     #[Route('/', name: '_index')]
-    public function index(): Response
+    public function index(SortieRepository $sortieRepository): Response
     {
+        $sorties = $sortieRepository->findAll();
         return $this->render('accueil/index.html.twig', [
-            'controller_name' => 'AccueilController',
+            "sorties"=>$sorties
+        ]);
+    }
+
+    #[Route('/sortie/detail/{id}', name:'sortie_detail', requirements: ['id'=>'\d+'])]
+    public function detail(Sortie $id):response
+    {
+        return $this->render('sortie/detail', [
+            "sortie" => $id
         ]);
     }
 }
