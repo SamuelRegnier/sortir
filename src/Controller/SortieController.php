4f1cd647881c16793080dd36503305e2c23cf393
sregnier2022 <samuel.regnier2022@campus-eni.fr>
2022-10-14 16:14:28 +0200

    MAJ Controller création SortieController.php avec nombre participants

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -59,7 +59,9 @@ class SortieController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($inscription);
+            if (!$user->isAdministrateur()) {
+                $entityManager->persist($inscription);
+            }
             $entityManager->persist($sortie);
             $entityManager->flush();
             return $this->redirectToRoute('accueil_index');
