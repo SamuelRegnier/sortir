d591014d5179dd48d607e8b291ef5156dd8b712a
tmasse2022 <tony.masse2022@campus-eni.fr>
2022-10-18 16:50:56 +0200

    Fontionnalités tableau accueil

--- src/Form/SortieType.php
+++ src/Form/SortieType.php
@@ -11,6 +11,7 @@ use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\RadioType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -52,9 +53,7 @@ class SortieType extends AbstractType
                     'label'=>'Lieu :',
                 'constraints'=>[
                     new notBlank()
-                ]
-
-            ])
+                ]])
             ->add('selectionner',ChoiceType::class,[
                 'mapped' => false,
                 'expanded' => true,
