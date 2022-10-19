6a60cb9e6e3a9232929563d106beffcc3e9872c3
qhoyet2022 <quentin.hoyet2022@campus-eni.fr>
2022-10-19 16:56:28 +0200

    Maj style Twig login, Form Sortie, SortieRepository.php et AccueilController.php pour filtre

--- src/Form/SortieType.php
+++ src/Form/SortieType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Lieu;
 use App\Entity\Sortie;
+use App\Entity\Ville;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
@@ -62,7 +63,6 @@ class SortieType extends AbstractType
                     'Enregistrer' => true,
                     'Publier' => false,
                 ],
-
             ])
     ;}
 
