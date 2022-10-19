dfb0e16b5d5e23657af09d6d6c9634c0371b1ee3
tmasse2022 <tony.masse2022@campus-eni.fr>
2022-10-19 16:59:44 +0200

    ajout de la map sur le twig affichage sortie

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -7,6 +7,7 @@ use App\Entity\Inscription;
 use App\Entity\Lieu;
 use App\Entity\Participant;
 use App\Entity\Sortie;
+use App\Form\AnnulationSortieType;
 use App\Form\SortieType;
 use App\Repository\EtatRepository;
 use App\Repository\InscriptionRepository;
@@ -16,9 +17,11 @@ use App\Repository\SiteRepository;
 use App\Repository\SortieRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class SortieController extends AbstractController
 {
@@ -26,6 +29,7 @@ class SortieController extends AbstractController
 
     #[Route('/creer/sortie', name: 'sortie_creer')]
     public function creer(
+        HttpClientInterface $client,
         Request $request,
         EntityManagerInterface $entityManager,
         EtatRepository $etatRepository,
@@ -68,6 +72,31 @@ class SortieController extends AbstractController
             if (!$user->isAdministrateur()) {
                 $entityManager->persist($inscription);
             }
+
+            $detailLieu = $request->get("sortie")["lieux"];
+            $lieu = $lieuRepository->find($detailLieu);
+//            dd($lieu);
+            $nomLieu = $lieu->getNom();
+//            dd($nomLieu);
+            $rueLieu = $lieu->getRue();
+//            dd($rueLieu);
+
+            $this->client = $client;
+            $response = $this->client->request(
+                'GET',
+                'http://nominatim.openstreetmap.org/search?format=json&limit=1&q='.$rueLieu.' '.$nomLieu
+            );
+
+            $content = $response->toArray();
+
+//            $json=file_get_contents('http://nominatim.openstreetmap.org/search?format=json&limit=1&q='.$rueLieu.' '.$nomLieu);
+//            $obj = json_decode($json, true);
+            $latitude = $content[0]['lat'];
+//            dd($latitude);
+            $longitude = $content[0]['lon'];
+            $lieu->setLatitude($latitude);
+            $lieu->setlongitude($longitude);
+
             $entityManager->persist($sortie);
             $entityManager->flush();
             return $this->redirectToRoute('accueil_index');
@@ -147,39 +176,33 @@ class SortieController extends AbstractController
         ]);
     }
 
-    // Page annulation d'une sortie
-
-    #[Route('/sortie/annulation/{id}', name: 'sortie_annulation', requirements: ['id'=>'\d+'])]
-    public function SortieAnnulation(
-        Sortie $id,
-    ): Response
-    {
-        return $this->render('sortie/annulation.html.twig', [
-            "sortie" => $id,
-        ]);
-    }
-
-    // Annuler une sortie
-
     #[Route('/sortie/annulee/{id}', name: 'sortie_annulee', requirements: ['id'=>'\d+'])]
     public function SortieAnnulee(
         Sortie $id,
         SortieRepository $SortieRepository,
         EtatRepository $etatRepository,
         InscriptionRepository $inscriptionRepository,
-        EntityManagerInterface $entityManager
+        EntityManagerInterface $entityManager,
+        Request $request
     ): Response
     {
         $user = $this->getUser();
         $sortie = $SortieRepository->find($id);
         $etat = $etatRepository->findOneBy(array('id'=> 6));
 
-        if ($sortie->getOrganisateur() === $user) {
-            $sortie->setEtats($etat);
-            $entityManager->persist($sortie);
-            $entityManager->flush();
-        }
-        return $this->redirectToRoute('accueil_index');
+        $formMotifAnnulation = $this->createForm(AnnulationSortieType::class,$sortie);
+        $formMotifAnnulation->handleRequest($request);
+
+        if ($sortie->getOrganisateur() === $user)
+            if ($formMotifAnnulation->isSubmitted() && $formMotifAnnulation->isValid()) {
+                $sortie->setEtats($etat);
+                $entityManager->persist($sortie);
+                $entityManager->flush();
+                return $this->redirectToRoute('accueil_index');
+            }
+        return $this->renderForm('sortie/annulation.html.twig',
+            compact('formMotifAnnulation', 'sortie')
+        );
     }
 
     // Afficher sortie annulée
